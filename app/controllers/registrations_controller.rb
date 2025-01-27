595f2af4b052f5780beb4e66f2c9001f7d837c78
Bobby Steckline <rsteckline@gmail.com>
2025-01-27 15:30:37 -0500

    Add more robust sign up form

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,7 +3,18 @@ class RegistrationsController < ApplicationController
         @user = User.new
     end
 
-    def create  
-        render plain: "Thanks!"
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            redirect_to root_path, notice: "Successfully created account"
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:email, :password, :password_confirmation)
     end
 end
