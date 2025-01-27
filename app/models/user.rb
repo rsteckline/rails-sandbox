0495d5106a0d827ad6c986f5aaf574ab2191c493
Bobby Steckline <rsteckline@gmail.com>
2025-01-27 14:53:49 -0500

    Add email regex validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,5 @@
 class User < ApplicationRecord
     has_secure_password
 
-    validates :email, presence: true
+    validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "Must be a valid email address" }
 end 
